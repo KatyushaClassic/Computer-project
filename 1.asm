eaf012f4facf90500fff125f2b7bf4eca3824a6c
KatyushaClassic <KatyushaClassic@github.com>
2026-05-29 23:41:29 +0800

    Fix rock detection

--- 1.asm
+++ 1.asm
@@ -1,5 +1,6 @@
 ; [改动 1] UpdateInputs: readKeys 后用 c（边沿），不用 b（按住）→ 每按一次方向键只走一格
 ; [改动 2] CheckMove Y 上界 + 下列常量 → 底部 UI_ROWS 行留给 UI，笑脸不可进入
+; [改动 3] 所有 tilemap 读写改为 WRAM 影子地图 (ShadowTilemap)，仅在 VBlank 同步到 VRAM
 INCLUDE "hardware.inc"
 
 DEF OBJCOUNT EQU 1
@@ -14,13 +15,9 @@ DEF OAM_Y_BIAS  EQU 16
 DEF PLAY_Y_MAX  EQU SCR_H - (UI_ROWS * TILE_PX) - SPRITE_PX + OAM_Y_BIAS
 DEF PLAY_Y_MIN  EQU OAM_Y_BIAS
 
-; 可见 20 列 × 16 行游玩区，底部 2 行 UI（LEVEL_H - UI_ROWS）
-DEF LEVEL_W     EQU 20
+DEF LEVEL_W     EQU 32
 DEF LEVEL_H     EQU 18
-DEF MAP_STRIDE  EQU 32          ; GB 背景 tilemap 每行 32 格（VRAM 行宽，关卡数据仍 20 列）
 DEF LEVEL_SIZE  EQU LEVEL_W * LEVEL_H
-DEF PLAY_X_MIN  EQU 8
-DEF PLAY_X_MAX  EQU 8 + (LEVEL_W * TILE_PX)
 
 DEF TILE_DIRT   EQU 0
 DEF TILE_ROCK   EQU 1
@@ -30,20 +27,8 @@ DEF TILE_EMPTY  EQU 5
 DEF TILE_START  EQU 6
 DEF TILE_UI     EQU 7
 DEF PLAY_ROWS   EQU LEVEL_H - UI_ROWS
-DEF UI_TEXT_ROW EQU LEVEL_H - 1
-DEF PRIZE_DIGIT_COL EQU 10
 
 CHARMAP " ", 7          ; 空格 → tile 7 (blank)
-CHARMAP "0", 9
-CHARMAP "1", 10
-CHARMAP "2", 11
-CHARMAP "3", 12
-CHARMAP "4", 13
-CHARMAP "5", 14
-CHARMAP "6", 15
-CHARMAP "7", 16
-CHARMAP "8", 17
-CHARMAP "9", 18
 CHARMAP "A", 19         ; A → tile 19
 CHARMAP "B", 20
 CHARMAP "C", 21
@@ -92,8 +77,8 @@ EntryPoint:
   ld     hl, ShadowOAM
   call   ResetOAM
   call   InitializeObjects
-  call   ResetBG
-  ld hl, TILEMAP0
+  call   ResetBG              ; [改动 3] 现在清空的是 ShadowTilemap
+  ld hl, ShadowTilemap        ; [改动 3] 标题文字写入 ShadowTilemap（原 TILEMAP0）
   ld de, PressStr
   ld b, PressStr.end - PressStr
 .copy:
@@ -102,6 +87,10 @@ EntryPoint:
   ld [hl+],a
   dec b
   jr nz, .copy
+
+  ; [改动 3] 修复白屏：去掉了这里的 WaitVBlank（LCD 关闭时 rLY 不递增会死循环）
+  ; LCD 已关闭，此时写 VRAM 绝对安全，直接同步即可
+  call CopyShadowTilemapToVRAM
 
   ld a, LCDC_ON | LCDC_OBJ_ON | LCDC_BG_ON | LCDC_BLOCK01
   ld [rLCDC], a
@@ -112,15 +101,18 @@ EntryPoint:
   call WaitVBlank
   ld a, 0
   ld [rLCDC], a
-  call   LoadLevel
+  call   LoadLevel            ; [改动 3] 关卡数据加载到 ShadowTilemap
+  call CopyShadowTilemapToVRAM; [改动 3] 开屏前同步到 VRAM
   ld a, LCDC_ON | LCDC_OBJ_ON | LCDC_BG_ON | LCDC_BLOCK01
   ld [rLCDC], a
 
 
 MainLoop:
-  call UpdateInputs
+  call UpdateInputs           ; [改动 3] 逻辑操作 ShadowTilemap（WRAM，随时安全）
+  call RockCheck              ; [改动 3] 逻辑操作 ShadowTilemap（WRAM，随时安全）
   call WaitVBlank
   call CopyShadowOAMtoOAM
+  ; [改动 4] 删除了耗时极长的全量复制 call CopyShadowTilemapToVRAM，改为按需局部更新
   jp MainLoop
 
 
@@ -133,7 +125,7 @@ WaitKey:
  ret
 
 ResetBG:
-  ld hl,TILEMAP0
+  ld hl,ShadowTilemap         ; [改动 3] 清空 ShadowTilemap（原 TILEMAP0）
   ld bc,1024
 .loop:
   ld [hl],5 ; tile 5 = 空地（黑底）
@@ -184,23 +176,38 @@ CopyShadowOAMtoOAM:
   jr nz, .loop
   ret
 
+; [改动 3] 新增：将 ShadowTilemap (WRAM) 整体复制到 VRAM TILEMAP0
+CopyShadowTilemapToVRAM:
+  ld hl, ShadowTilemap
+  ld de, TILEMAP0
+  ld bc, 1024
+.loop:
+  ld a, [hl+]
+  ld [de], a
+  inc de
+  dec bc
+  ld a, b
+  or c
+  jr nz, .loop
+  ret
+
 UpdateInputs:
   ld hl,ShadowOAM       ; 人物素材坐标
   push hl
   call readKeys
   ld a,c                ; [改动 1] 原版: ld a,b
   bit 5,a               ; 左键
-  jp nz, .moveLeft
+  jr nz, .moveLeft
   bit 6,a               ; 上键
-  jp nz, .moveUp
+  jr nz, .moveUp
   bit 4,a               ; 右键
-  jp nz, .moveRight
+  jr nz, .moveRight
   bit 7,a               ; 下键
-  jp nz, .moveDown
+  jr nz, .moveDown
 
   ; 待添加：reset 功能和换关功能
 
-  jp .next
+  jr .next
 .moveDown
   inc [hl]
   inc [hl]
@@ -214,15 +221,7 @@ UpdateInputs:
   ld a,1             ; a=1:Y   a=0:X
   call CheckMove
   cp 1
-  jr z,.blocked
-
-  call TryCollectMoneyAtPlayerTile
-  jp .next
-
-.blocked:
-  call DigIfDirtAtPlayerTile
-  cp 0
-  jp z,.next
+  jr nz,.next
 
   dec [hl]
   dec [hl]
@@ -232,7 +231,8 @@ UpdateInputs:
   dec [hl]
   dec [hl]
   dec [hl]
-  jp .next
+
+  jr .next
 
 .moveLeft
   inc hl
@@ -248,19 +248,7 @@ UpdateInputs:
   ld a,0
   call CheckMove
   cp 1
-  jr z,.blockedLeft
-
-  call TryCollectMoneyAtPlayerTile
-  jp .next
-
-.blockedLeft:
-  call TryPushRockLeftAtPlayerTile
-  cp 0
-  jp z,.next
-
-  call DigIfDirtAtPlayerTile
-  cp 0
-  jp z,.next
+  jr nz,.next
 
   inc [hl]
   inc [hl]
@@ -270,7 +258,8 @@ UpdateInputs:
   inc [hl]
   inc [hl]
   inc [hl]
-  jp .next
+
+  jr .next
 
 .moveUp
   dec [hl]
@@ -285,15 +274,7 @@ UpdateInputs:
   ld a,1
   call CheckMove
   cp 1
-  jr z,.blockedUp
-
-  call TryCollectMoneyAtPlayerTile
-  jp .next
-
-.blockedUp:
-  call DigIfDirtAtPlayerTile
-  cp 0
-  jp z,.next
+  jr nz,.next
 
   inc [hl]
   inc [hl]
@@ -303,7 +284,8 @@ UpdateInputs:
   inc [hl]
   inc [hl]
   inc [hl]
-  jp .next
+
+  jr .next
 
 .moveRight
   inc hl
@@ -319,19 +301,7 @@ UpdateInputs:
   ld a,0
   call CheckMove
   cp 1
-  jr z,.blockedRight
-
-  call TryCollectMoneyAtPlayerTile
-  jp .next
-
-.blockedRight:
-  call TryPushRockRightAtPlayerTile
-  cp 0
-  jp z,.next
-
-  call DigIfDirtAtPlayerTile
-  cp 0
-  jp z,.next
+  jr nz,.next
 
   dec [hl]
   dec [hl]
@@ -341,176 +311,10 @@ UpdateInputs:
   dec [hl]
   dec [hl]
   dec [hl]
-  jp .next
+
+  jr .next
 
 .next
-  pop hl
-  ret
-
-; 输出 b=行 c=列（玩家脚下格）
-GetPlayerTilePos:
-  ld a,[ShadowOAM]
-  sub OAM_Y_BIAS
-  srl a
-  srl a
-  srl a
-  ld b,a
-  ld a,[ShadowOAM + 1]
-  sub 8
-  srl a
-  srl a
-  srl a
-  ld c,a
-  ret
-
-; 输入 b=行 c=列，输出 hl -> TILEMAP0 对应格
-GetTilemapPtrBC:
-  ld a,b
-  ld l,a
-  ld h,0
-  add hl,hl
-  add hl,hl
-  add hl,hl
-  add hl,hl
-  add hl,hl
-  ld a,c
-  ld e,a
-  ld d,0
-  add hl,de
-  ld de,TILEMAP0
-  add hl,de
-  ret
-
-; 玩家 OAM 已在目标格时：若是土则挖掉并留在该格（a=0），否则 a=1
-DigIfDirtAtPlayerTile:
-  push hl
-  push bc
-  call GetPlayerTilePos
-  ld a,b
-  cp PLAY_ROWS
-  jr nc,.fail
-  ld a,c
-  cp LEVEL_W
-  jr nc,.fail
-  call GetTilemapPtrBC
-  ld a,[hl]
-  cp TILE_DIRT
-  jr nz,.fail
-  ld a,TILE_EMPTY
-  ld [hl],a
-  ld a,0
-  jr .done
-.fail
-  ld a,1
-.done
-  pop bc
-  pop hl
-  ret
-
-; 站在金币格：清空背景格、prizeLeft--、刷新底部数字
-TryCollectMoneyAtPlayerTile:
-  push hl
-  push bc
-  call GetPlayerTilePos
-  ld a, b
-  cp PLAY_ROWS
-  jr nc, .done
-  call GetTilemapPtrBC
-  ld a, [hl]
-  cp TILE_MONEY
-  jr nz, .done
-  ld a, TILE_EMPTY
-  ld [hl], a
-  ld a, [prizeLeft]
-  and a
-  jr z, .done
-  dec a
-  ld [prizeLeft], a
-  call UpdatePrizeDigit
-.done
-  pop bc
-  pop hl
-  ret
-
-; 玩家已移动到目标格（该格被阻挡）时，尝试把该格 rock 向左推一格
-; 成功 a=0（玩家保留当前位置），失败 a=1
-TryPushRockLeftAtPlayerTile:
-  push hl
-  push bc
-  call GetPlayerTilePos
-  ld a,b
-  cp PLAY_ROWS
-  jr nc,.fail
-  ld a,c
-  cp LEVEL_W
-  jr nc,.fail
-  call GetTilemapPtrBC
-  ld a,[hl]
-  cp TILE_ROCK
-  jr nz,.fail
-
-  ld a,c
-  and a
-  jr z,.fail            ; 已在最左列，不能再推
-  dec c
-  call GetTilemapPtrBC
-  ld a,[hl]
-  cp TILE_EMPTY
-  jr nz,.fail
-  ld a,TILE_ROCK
-  ld [hl],a
-
-  inc c
-  call GetTilemapPtrBC
-  ld a,TILE_EMPTY
-  ld [hl],a
-  ld a,0
-  jr .done
-.fail
-  ld a,1
-.done
-  pop bc
-  pop hl
-  ret
-
-; 玩家已移动到目标格（该格被阻挡）时，尝试把该格 rock 向右推一格
-; 成功 a=0（玩家保留当前位置），失败 a=1
-TryPushRockRightAtPlayerTile:
-  push hl
-  push bc
-  call GetPlayerTilePos
-  ld a,b
-  cp PLAY_ROWS
-  jr nc,.fail
-  ld a,c
-  cp LEVEL_W
-  jr nc,.fail
-  call GetTilemapPtrBC
-  ld a,[hl]
-  cp TILE_ROCK
-  jr nz,.fail
-
-  ld a,c
-  cp LEVEL_W - 1
-  jr nc,.fail           ; 已在最右列，不能再推
-  inc c
-  call GetTilemapPtrBC
-  ld a,[hl]
-  cp TILE_EMPTY
-  jr nz,.fail
-  ld a,TILE_ROCK
-  ld [hl],a
-
-  dec c
-  call GetTilemapPtrBC
-  ld a,TILE_EMPTY
-  ld [hl],a
-  ld a,0
-  jr .done
-.fail
-  ld a,1
-.done
-  pop bc
   pop hl
   ret
 
@@ -528,9 +332,9 @@ CheckMove:
 
 .XCoordinate
   ld a,[hl]
-  cp PLAY_X_MAX
+  cp 160+4
   jr nc,.WithdrawMove
-  cp PLAY_X_MIN
+  cp 8
   jr c,.WithdrawMove
 
 .Boundarydetectioncompleted
@@ -567,20 +371,48 @@ CheckMove:
   ld e, a
   ld d, 0
   add hl, de
-  ld bc, TILEMAP0
+  push hl             ; [改动 3] 保存 tilemap 索引到栈
+  ld bc, ShadowTilemap  ; [改动 3] 碰撞检测读 ShadowTilemap（原 TILEMAP0）
   add hl, bc
-  ld a, [hl]
+  ld a, [hl]          ; a = tile value
+  pop hl              ; [改动 3] 恢复索引到 hl（供后续写入使用）
   pop bc
 
   cp TILE_EMPTY
   jr z, .MoveAllowed
+
   cp TILE_START
   jr z, .MoveAllowed
   cp TILE_MONEY
-  jr z, .MoveAllowed
+  ;待添加：金币计数器
+  jr z, .MoveAllowedAndChangeBG
+  cp TILE_DIRT
+  jr z, .MoveAllowedAndChangeBG
+  
   jr .TileBlocked
+ 
 
 .MoveAllowed
+  pop hl
+  ld a, 0
+  ret
+  
+.MoveAllowedAndChangeBG    ;改成空
+  push bc                  ; [改动 3] 保护 bc
+  ld bc, ShadowTilemap     ; [改动 3] hl(索引) + bc(基址) = WRAM 地址
+  add hl, bc
+  ld a, TILE_EMPTY
+  ld [hl],a                ; [改动 3] 修复白屏/吃土失效：现在正确写入 ShadowTilemap
+
+  ; [改动 4] 新增：将改动局部同步更新至 VRAM
+  push hl
+  ld bc, TILEMAP0 - ShadowTilemap
+  add hl, bc
+  call WriteVRAMSafe
+  pop hl
+  ; [改动 4] 结束
+
+  pop bc                  ; [改动 3] 恢复 bc
   pop hl
   ld a, 0
   ret
@@ -599,52 +431,114 @@ CheckMove:
   ld a, 1
   ret
 
-CountMoneyInLevel:
-  ld hl, Level1Map
-  ld bc, LEVEL_SIZE
-  ld d, 0
-.loop:
-  ld a, [hl+]
-  cp TILE_MONEY
-  jr nz, .skip
-  inc d
-.skip
+; [改动 4] 彻底重构了 RockCheck 逻辑，极大优化指令周期并按需更新 VRAM
+RockCheck:
+  ; 只扫描实际游戏区域，跳过无用的底层内存。从倒数第二行开始扫描
+  ld hl, ShadowTilemap + LEVEL_SIZE - 1 - LEVEL_W 
+  ld bc, LEVEL_SIZE - LEVEL_W
+
+.SearchRockLoop:
+  ld a, [hld]        ; 利用 hld 自动递减
+  cp TILE_ROCK
+  jr nz, .NextTile
+
+.MoveRock:
+  inc hl             ; 补回 hld 多减的 1
+  ld d, h            ; de 保存原石头地址
+  ld e, l
+
+  push bc            ; 保护主循环计数器
+
+  ld bc, LEVEL_W
+  add hl, bc         ; hl 现在是正下方的地址
+
+  call CheckRockMove
+  cp 0               ; 0 表示可以移动（CheckRockMove 内部已将其变成石头）
+  jr z, .RockMoved
+
+  ; 检测左下 (hl - 1)
+  dec hl
+  call CheckRockMove
+  cp 0
+  jr z, .RockMoved
+
+  ; 检测右下 (刚才减了1，现在要加2才能到右边)
+  inc hl
+  inc hl
+  call CheckRockMove
+  cp 0
+  jr z, .RockMoved
+
+  ; 都不行，石头不能动
+  ld h, d            ; 恢复 hl 指向原石头
+  ld l, e
+  pop bc             ; 恢复计数器
+  dec hl             ; 移到前一个 tile 以继续循环
+  jp .NextTile       ; [改动 4] 距离可能过长，采用 jp 确保不越界
+
+.RockMoved:
+  ; 石头已经掉下去了，把老位置清空
+  ld h, d
+  ld l, e
+  ld a, TILE_EMPTY
+  ld [hl], a         ; [改动 4] 更新 WRAM 原位置为空
+
+  ; [改动 4] 新增：同步更新 VRAM 的老位置为空
+  push hl
+  push bc
+  ld bc, TILEMAP0 - ShadowTilemap
+  add hl, bc
+  call WriteVRAMSafe
+  pop bc
+  pop hl
+  ; [改动 4] 结束
+
+  pop bc             ; 恢复计数器
+  dec hl             ; 移到前一个 tile 以继续循环
+
+.NextTile:
   dec bc
   ld a, b
   or c
-  jr nz, .loop
-  ld a, d
-  ld [prizeLeft], a
+  jp nz, .SearchRockLoop
+  ret
+  
+; [改动 4] 优化了 CheckRockMove，精简冗余判断并按需更新 VRAM
+CheckRockMove:        
+  ld a, [hl]
+  
+  cp TILE_EMPTY
+  jr z, .MoveAllowedAndChangeBG
+  
+  cp PLAYER_TILE_ID
+  jr z, .Die
+  
+  ; 不是空地和玩家，统统视为阻挡
+  ld a, 1            
   ret
 
-UpdatePrizeDigit:
-  ld a, [prizeLeft]
-  add a, 9
-  ld hl, TILEMAP0
-  ld bc, UI_TEXT_ROW * MAP_STRIDE + PRIZE_DIGIT_COL
+.MoveAllowedAndChangeBG:
+  ld a, TILE_ROCK
+  ld [hl], a         ; [改动 4] 写入 WRAM 新位置为石头
+
+  ; [改动 4] 新增：同步更新 VRAM 新位置为石头
+  push hl
+  ld bc, TILEMAP0 - ShadowTilemap
   add hl, bc
-  ld [hl], a
+  call WriteVRAMSafe
+  pop hl
+  ; [改动 4] 结束
+
+  xor a              ; 相当于 ld a, 0
   ret
 
-DrawPrizeUI:
-  ld hl, TILEMAP0
-  ld bc, UI_TEXT_ROW * MAP_STRIDE
-  add hl, bc
-  ld de, PrizeUiLine
-  ld b, PrizeUiLine.end - PrizeUiLine
-.copy:
-  ld a, [de]
-  inc de
-  ld [hl+], a
-  dec b
-  jr nz, .copy
-  call UpdatePrizeDigit
+.Die:
+  ld a, 1            
   ret
-
+  
 LoadLevel:
-  call CountMoneyInLevel
   ld hl, Level1Map
-  ld de, TILEMAP0
+  ld de, ShadowTilemap       ; [改动 3] 关卡加载到 ShadowTilemap（原 TILEMAP0）
   xor a
   ld b, a
 .row:
@@ -676,26 +570,20 @@ LoadLevel:
   ld a, c
   cp LEVEL_W
   jr nz, .col
-  ld a, MAP_STRIDE - LEVEL_W
-.pad:
-  inc de
-  dec a
-  jr nz, .pad
   inc b
   ld a, b
   cp LEVEL_H
   jr nz, .row
 
-  ld bc, 1024 - (LEVEL_H * MAP_STRIDE)
+  ld hl, ShadowTilemap + LEVEL_SIZE  ; [改动 3] 填充剩余（原 TILEMAP0）
+  ld bc, 1024 - LEVEL_SIZE
 .fillRest:
   ld a, TILE_EMPTY
-  ld [de], a
-  inc de
+  ld [hl+], a
   dec bc
   ld a, b
   or c
   jr nz, .fillRest
-  call DrawPrizeUI
   ret
 
 Random2bits:
@@ -784,37 +672,43 @@ readKeys:
   ldh   [rP1],a
   ret
 
+; [改动 4] 新增：安全写入 VRAM 的辅助函数
+WriteVRAMSafe:
+  push af
+.waitSTAT
+  ldh a, [rSTAT]
+  and 2             ; 检测 LCD 是否在读取 VRAM (Mode 2 或 3)
+  jr nz, .waitSTAT  ; 如果是，则等待安全周期
+  pop af
+  ld [hl], a        ; 抓准时机，安全写入
+  ret
+
+
 SECTION "Data", ROM0
 PressStr:
   DB "PRESS ANY KEY"
 .end
 
-PrizeUiLine:
-  DB "PRIZELEFT "
-.end
 
-
-; 关卡 20 列满宽（无左右墙列）；竖垛列 4/8/12/16：dirt→rock→money→wall
-; 行 0/15 顶底墙；行 16–17 UI
 Level1Map:
-  DB 4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4
-  DB 5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5
-  DB 5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5
-  DB 5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5
-  DB 5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5
-  DB 5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5
-  DB 5,6,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5
-  DB 5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5
-  DB 5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5
-  DB 5,5,5,5,0,5,5,5,0,5,5,5,0,5,5,5,0,5,5,5
-  DB 5,5,5,5,1,5,5,5,1,5,5,5,1,5,5,5,1,5,5,5
-  DB 5,5,5,5,3,5,5,5,3,5,5,5,3,5,5,5,3,5,5,5
-  DB 5,5,5,5,4,5,5,5,4,5,5,5,4,5,5,5,4,5,5,5
-  DB 5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5
-  DB 5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5
-  DB 4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4
-  DB 7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7
-  DB 7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7
+  DB 4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4
+  DB 4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,4
+  DB 4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,4
+  DB 4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,4
+  DB 4,5,5,5,5,5,5,5,5,1,5,5,5,5,5,5,5,5,1,5,5,5,5,5,5,5,5,5,5,5,5,4
+  DB 4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,4
+  DB 4,5,5,0,0,0,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,4
+  DB 4,5,5,5,5,5,5,5,5,3,3,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,4
+  DB 4,5,5,5,5,5,5,5,5,5,5,5,5,5,3,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,4
+  DB 4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,4
+  DB 4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,4
+  DB 4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,4
+  DB 4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,4
+  DB 4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,4
+  DB 4,5,6,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,4
+  DB 4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4
+  DB 7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7
+  DB 7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7
 
 
 Tiles:
@@ -893,48 +787,48 @@ DB %00000000,%00000000,%00000000,%00000000,%00000000,%00000000,%00000000,%000000
 ;8
 DB %00000000,%00000000,%00000000,%00000000,%00000000,%00000000,%00000000,%00000000
 ; digits and letters
- DB $00,$3C,$66,$66,$66,$66,$3C,$00 ; 0
- DB $00,$18,$38,$18,$18,$18,$3C,$00 ; 1
- DB $00,$3C,$4E,$0E,$3C,$70,$7E,$00
- DB $00,$7C,$0E,$3C,$0E,$0E,$7C,$00
- DB $00,$3C,$6C,$4C,$4E,$7E,$0C,$00
- DB $00,$7C,$60,$7C,$0E,$4E,$3C,$00
- DB $00,$3C,$60,$7C,$66,$66,$3C,$00
- DB $00,$7E,$06,$0C,$18,$38,$38,$00
- DB $00,$3C,$4E,$3C,$4E,$4E,$3C,$00 ; 8
- DB $00,$3C,$4E,$4E,$3E,$0E,$3C,$00 ; 9
- DB $00,$3C,$4E,$4E,$7E,$4E,$4E,$00 ; A
- DB $00,$7C,$66,$7C,$66,$66,$7C,$00 ; B
- DB $00,$3C,$66,$60,$60,$66,$3C,$00 ; C
- DB $00,$7C,$4E,$4E,$4E,$4E,$7C,$00
- DB $00,$7E,$60,$7C,$60,$60,$7E,$00
- DB $00,$7E,$60,$60,$7C,$60,$60,$00
- DB $00,$3C,$66,$60,$6E,$66,$3E,$00
- DB $00,$46,$46,$7E,$46,$46,$46,$00
- DB $00,$3C,$18,$18,$18,$18,$3C,$00
- DB $00,$1E,$0C,$0C,$6C,$6C,$38,$00
- DB $00,$66,$6C,$78,$78,$6C,$66,$00
- DB $00,$60,$60,$60,$60,$60,$7E,$00
- DB $00,$46,$6E,$7E,$56,$46,$46,$00
- DB $00,$46,$66,$76,$5E,$4E,$46,$00
- DB $00,$3C,$66,$66,$66,$66,$3C,$00
- DB $00,$7C,$66,$66,$7C,$60,$60,$00
- DB $00,$3C,$62,$62,$6A,$64,$3A,$00
- DB $00,$7C,$66,$66,$7C,$68,$66,$00
- DB $00,$3C,$60,$3C,$0E,$4E,$3C,$00
- DB $00,$7E,$18,$18,$18,$18,$18,$00
- DB $00,$46,$46,$46,$46,$4E,$3C,$00
- DB $00,$46,$46,$46,$46,$2C,$18,$00
- DB $00,$46,$46,$56,$7E,$6E,$46,$00
- DB $00,$46,$2C,$18,$38,$64,$42,$00  ; X
- DB $00,$66,$66,$3C,$18,$18,$18,$00  ; Y
- DB $00,$7E,$0E,$1C,$38,$70,$7E,$00  ; Z
- DB $00,$00,$00,$00,$00,$60,$60,$00  ; .
- DB $00,$00,$00,$3C,$3C,$00,$00,$00  ; -
+  DB $00,$3C,$66,$66,$66,$66,$3C,$00 ; 0
+  DB $00,$18,$38,$18,$18,$18,$3C,$00 ; 1
+  DB $00,$3C,$4E,$0E,$3C,$70,$7E,$00
+  DB $00,$7C,$0E,$3C,$0E,$0E,$7C,$00
+  DB $00,$3C,$6C,$4C,$4E,$7E,$0C,$00
+  DB $00,$7C,$60,$7C,$0E,$4E,$3C,$00
+  DB $00,$3C,$60,$7C,$66,$66,$3C,$00
+  DB $00,$7E,$06,$0C,$18,$38,$38,$00
+  DB $00,$3C,$4E,$3C,$4E,$4E,$3C,$00 ; 8
+  DB $00,$3C,$4E,$4E,$3E,$0E,$3C,$00 ; 9
+  DB $00,$3C,$4E,$4E,$7E,$4E,$4E,$00 ; A
+  DB $00,$7C,$66,$7C,$66,$66,$7C,$00 ; B
+  DB $00,$3C,$66,$60,$60,$66,$3C,$00 ; C
+  DB $00,$7C,$4E,$4E,$4E,$4E,$7C,$00
+  DB $00,$7E,$60,$7C,$60,$60,$7E,$00
+  DB $00,$7E,$60,$60,$7C,$60,$60,$00
+  DB $00,$3C,$66,$60,$6E,$66,$3E,$00
+  DB $00,$46,$46,$7E,$46,$46,$46,$00
+  DB $00,$3C,$18,$18,$18,$18,$3C,$00
+  DB $00,$1E,$0C,$0C,$6C,$6C,$38,$00
+  DB $00,$66,$6C,$78,$78,$6C,$66,$00
+  DB $00,$60,$60,$60,$60,$60,$7E,$00
+  DB $00,$46,$6E,$7E,$56,$46,$46,$00
+  DB $00,$46,$66,$76,$5E,$4E,$46,$00
+  DB $00,$3C,$66,$66,$66,$66,$3C,$00
+  DB $00,$7C,$66,$66,$7C,$60,$60,$00
+  DB $00,$3C,$62,$62,$6A,$64,$3A,$00
+  DB $00,$7C,$66,$66,$7C,$68,$66,$00
+  DB $00,$3C,$60,$3C,$0E,$4E,$3C,$00
+  DB $00,$7E,$18,$18,$18,$18,$18,$00
+  DB $00,$46,$46,$46,$46,$4E,$3C,$00
+  DB $00,$46,$46,$46,$46,$2C,$18,$00
+  DB $00,$46,$46,$56,$7E,$6E,$46,$00
+  DB $00,$46,$2C,$18,$38,$64,$42,$00  ; X
+  DB $00,$66,$66,$3C,$18,$18,$18,$00  ; Y
+  DB $00,$7E,$0E,$1C,$38,$70,$7E,$00  ; Z
+  DB $00,$00,$00,$00,$00,$60,$60,$00  ; .
+  DB $00,$00,$00,$3C,$3C,$00,$00,$00  ; -
 TilesEnd:
 
 SECTION "Variables", WRAM0
 ShadowOAM: DS 160
 current: DS 1
 previous: DS 1
-prizeLeft: DS 1
+ShadowTilemap: DS 1024       ; [改动 3] 新增：tilemap 的 WRAM 影子（1024 字节）
